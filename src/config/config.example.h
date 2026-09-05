895ca782248fcd2bce9e4fe840fc7548811dada2
Cedric Poirson <42615723+CedricPoirson@users.noreply.github.com>
2026-09-05 09:42:01 +0200

    Clean duplicate configuration macros for LilyGO build

--- src/config/config.example.h
+++ src/config/config.example.h
@@ -1,187 +1,106 @@
 #ifndef CONFIG
 #define CONFIG
 
-/**
- * WiFi credentials
- */
+// -----------------------------------------------------------------------------
+// Réseau
+// -----------------------------------------------------------------------------
 #define WEBSSERVER true
-#define WIFI_ACTIVE  true
+#define WIFI_ACTIVE true
 #define MQTT_CLIENT true
 
 #define WIFI_NETWORK "YOUR_WIFI_SSID"
 #define WIFI_PASSWORD "YOUR_WIFI_PASSWORD"
 
-/**
- * MQTT credentials
- */
-
-#define MQTT_USER     "YOUR_MQTT_USER"
+#define MQTT_PORT 1883
+#define MQTT_USER "YOUR_MQTT_USER"
 #define MQTT_PASSWORD "YOUR_MQTT_PASSWORD"
 
-/**
- * Switch Screnn button and time on
- */
+// -----------------------------------------------------------------------------
+// Matériel utilisé
+// -----------------------------------------------------------------------------
+#define DEVICE_NAME "PVRouter ESP32"
+#define DEVICE_ID "pvrouter-esp32"
+#define DEVICE_MODEL "LilyGO T-Display"
+#define DEVICE_MANUF "Cédric Poirson"
+#define DEVICE_VERSION "1.0"
+
 #define SWITCH 35
-//#define SWITCHTIMER 0   // 0 : always ON  / other : time in sec 
-
-
 #define IP_FRONIUS "192.168.100.245"
-
-
-/**
- * Time between 2 mesure of dimmer temp
- */
 #define GETTEMPREFRESH 30
-
-
-/**
- * Set this to false to disable Serial logging
- */
 #define DEBUG true
-//#define configMAX_PRIORITIES 1024
 
-
-/**
- * The name of this device (as defined in the AWS IOT console).
- * Also used to set the hostname on the network
- */
-#define DEVICE_NAME "Pv router ESP32"
-
-/**
- * ADC input pin that is used to read out the CT sensor
- */
-
-#ifdef  DEVKIT1
-#define ADC_INPUT 32 // linky
-#define ADC_PORTEUSE 33 // porteuse
+#ifdef DEVKIT1
+#define ADC_INPUT 32
+#define ADC_PORTEUSE 33
 #endif
 
-#ifdef  TTGO
-#define ADC_INPUT 32 // linky
-#define ADC_PORTEUSE 33 // porteuse
+#ifdef TTGO
+#define ADC_INPUT 32
+#define ADC_PORTEUSE 33
 #endif
 
-
-#define ADC_MIDDLE 1893  /// en dessous laquelle on considère une valeur négative
-
-/**
- * The voltage of your home, used to calculate the wattage.
- * Try setting this as accurately as possible.
- */
+#define ADC_MIDDLE 1893
 #define HOME_VOLTAGE 225.0
 
-/**
- *  Dimmer 
- */
-
+// -----------------------------------------------------------------------------
+// Dimmer
+// -----------------------------------------------------------------------------
 #define DIMMER true
 #define DIMMERLOCAL false
 #define DALLAS false
 
-#if DIMMERLOCAL 
-#define outputPin  26 
-#define zerocross  27 // for boards with CHANGEBLE input pins
+#if DIMMERLOCAL
+#define outputPin 26
+#define zerocross 27
 #endif
 
 #if DALLAS
-#define dallaspin  37
+#define dallaspin 37
 #endif
 
+// -----------------------------------------------------------------------------
+// Wi-Fi
+// -----------------------------------------------------------------------------
+#define WIFI_TIMEOUT 20000
+#define WIFI_RECOVER_TIME_MS 20000
 
-
-/**
- * Timeout for the WiFi connection. When this is reached,
- * the ESP goes into deep sleep for 30seconds to try and
- * recover.
- */
-#define WIFI_TIMEOUT 20000 // 20 seconds
-
-/**
- * How long should we wait after a failed WiFi connection
- * before trying to set one up again.
- */
-#define WIFI_RECOVER_TIME_MS 20000 // 20 seconds
-
-/**
- * Dimensions of the OLED display attached to the ESP
- */
+// -----------------------------------------------------------------------------
+// Affichage
+// -----------------------------------------------------------------------------
 #define OLED_ON true
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 
-/**
- * Force Emonlib to assume a 3.3V supply to the CT sensor
- */
-#define emonTxV3 1
-
-
-/**
- * Local measurements
- */
-#define LOCAL_MEASUREMENTS 30
-#define FACTEURPUISSANCE 10.50
-
-/**
- * Syncing time with an NTP server
- */
-#define NTP_TIME_SYNC_ENABLED true
-#define NTP_SERVER "europe.pool.ntp.org"
-#define NTP_OFFSET_SECONDS 3600
-#define NTP_UPDATE_INTERVAL_MS 3600000 /// synch de l'heure toute les heures
-
-
-//*************not implemented /tested **********
-/**
- * Wether or not you want to enable Home Assistant integration
- */
-//#define HA_ENABLED false
-//#define HA_ADDRESS "*** YOUR HOME ASSISTANT IP ADDRESSS ***"
-//#define HA_PORT 8883
-//#define HA_USER ""
-//#define HA_PASSWORD ""
-//*************not implemented /tested **********
-/**
- * The MQTT endpoint of the service we should connect to and receive messages
- * from.
- */
-//#define AWS_ENABLED false
-//#define AWS_IOT_ENDPOINT "**** YOUR AWS IOT ENDPOINT ****"
-//#define AWS_IOT_TOPIC "**** YOUR AWS IOT RULE ARN ****"
-
-//#define MQTT_CONNECT_DELAY 200
-//#define MQTT_CONNECT_TIMEOUT 20000 // 20 seconds
-//*************END not implemented /tested **********
-
-// Check which core Arduino is running on. This is done because updating the 
-// display only works from the Arduino core.
 #if CONFIG_FREERTOS_UNICORE
 #define ARDUINO_RUNNING_CORE 0
 #else
 #define ARDUINO_RUNNING_CORE 1
 #endif
 
+// -----------------------------------------------------------------------------
+// Ancienne mesure locale - conservée temporairement pour compatibilité
+// -----------------------------------------------------------------------------
+#define emonTxV3 1
+#define LOCAL_MEASUREMENTS 30
+#define FACTEURPUISSANCE 10.50
+
+// -----------------------------------------------------------------------------
+// Heure
+// NTP_OFFSET_SECONDS sert uniquement d'offset initial à NTPClient.
+// La tâche NTP applique ensuite automatiquement UTC+1 / UTC+2 pour Paris.
+// -----------------------------------------------------------------------------
+#define NTP_TIME_SYNC_ENABLED true
+#define NTP_SERVER "europe.pool.ntp.org"
+#define NTP_OFFSET_SECONDS 3600
+#define NTP_UPDATE_INTERVAL_MS 3600000
+
+// -----------------------------------------------------------------------------
+// Options historiques non utilisées dans la configuration actuelle
+// -----------------------------------------------------------------------------
+//#define HA_ENABLED false
+//#define AWS_ENABLED false
+
 #define VERSION "version 3.6"
-
-
-// Informations générales MQTT
-#define MQTT_SERVER     "192.168.100.20"
-#define MQTT_PORT       1883
-#define MQTT_USER       "esp32"
-#define MQTT_PASSWORD   "esp32"
-
-// Nom du périphérique MQTT (utilisé dans discovery)
-#define DEVICE_NAME     "PVRouter ESP32"
-#define DEVICE_ID       "pvrouter-esp32"
-#define DEVICE_MODEL    "TTGO T-Display"
-#define DEVICE_MANUF    "Cédric Poirson"
-#define DEVICE_VERSION  "1.0"
-
-// Préfixe du topic discovery Home Assistant
 #define HA_DISCOVERY_PREFIX "homeassistant"
-
-// Topics dynamiques générés dans le code (exemples) :
-// - state_topic : homeassistant/sensor/<sensor>/state
-// - config_topic : homeassistant/sensor/<sensor>/config
-// - status_topic : homeassistant/sensor/<sensor>/status
 
 #endif
