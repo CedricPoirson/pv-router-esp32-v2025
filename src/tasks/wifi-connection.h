4318902e025e32273747b1aecbcc3eabcbf2ce7b
Cedric Poirson <42615723+CedricPoirson@users.noreply.github.com>
2026-09-06 15:02:07 +0200

    Remove routine WiFi task debug line

--- src/tasks/wifi-connection.h
+++ src/tasks/wifi-connection.h
@@ -6,23 +6,15 @@
 #include "../config/enums.h"
 #include "../config/config.h"
 
-
-
 extern DisplayValues gDisplayValues;
 extern void goToDeepSleep();
-extern Configwifi configwifi; 
+extern Configwifi configwifi;
+
 /**
- * Task: monitor the WiFi connection and keep it alive!
- * 
- * When a WiFi connection is established, this task will check it every 30 seconds 
- * to make sure it's still alive.
- * 
- * If not, a reconnect is attempted. If this fails to finish within the timeout,
- * the ESP32 is send to deep sleep in an attempt to recover from this.
+ * Task: monitor the WiFi connection and keep it alive.
  */
 void keepWiFiAlive(void * parameter){
     for(;;){
-        serial_println(F("Wifi task"));
         if(WiFi.status() == WL_CONNECTED){
             vTaskDelay(30000 / portTICK_PERIOD_MS);
             continue;
@@ -35,15 +27,12 @@ void keepWiFiAlive(void * parameter){
         WiFi.setHostname(DEVICE_NAME);
         if ( strcmp(WIFI_NETWORK,"xxx") == 0) { WiFi.begin(configwifi.SID, configwifi.passwd); }
         else { WiFi.begin(WIFI_NETWORK, WIFI_PASSWORD); }
-        
 
         unsigned long startAttemptTime = millis();
 
-        // Keep looping while we're not connected and haven't reached the timeout
-        while (WiFi.status() != WL_CONNECTED && 
+        while (WiFi.status() != WL_CONNECTED &&
                 millis() - startAttemptTime < WIFI_TIMEOUT){}
 
-        // Make sure that we're actually connected, otherwise go to deep sleep
         if(WiFi.status() != WL_CONNECTED){
             serial_println(F("[WIFI] FAILED"));
             vTaskDelay(WIFI_RECOVER_TIME_MS / portTICK_PERIOD_MS);
