161b70bb9bea0a2588cc577af4ec9412d3be8fd6
Cedric Poirson <42615723+CedricPoirson@users.noreply.github.com>
2026-09-06 12:14:50 +0200

    Parse dimmer state for temperature and communication status

--- src/tasks/gettemp.h
+++ src/tasks/gettemp.h
@@ -12,44 +12,62 @@ HTTPClient httpdimmer;
 extern DisplayValues gDisplayValues;
 
 void GetDImmerTemp(void * parameter){
- for (;;){ 
-// create get request 
-    String baseurl; 
-      baseurl = "/state" ; 
-      httpdimmer.begin(String(config.dimmer),80,baseurl);   
-      int httpResponseCode = httpdimmer.GET();
+  for (;;){
+    String baseurl = "/state";
+    httpdimmer.begin(String(config.dimmer), 80, baseurl);
+    int httpResponseCode = httpdimmer.GET();
 
-      String dimmerstate = ""; 
+    String dimmerstate = "";
 
-//  read request return
-  if (httpResponseCode>0) {
-    Serial.print("HTTP Response code: ");
-    Serial.println(httpResponseCode);
-    dimmerstate = httpdimmer.getString();
+    if (httpResponseCode == HTTP_CODE_OK) {
+      Serial.print("HTTP Response code: ");
+      Serial.println(httpResponseCode);
+      dimmerstate = httpdimmer.getString();
+
+      // Expected payload example:
+      // 50;36.94;16;5;The_Wifi;-4
+      // POWER;temperature;...
+      int firstSeparator = dimmerstate.indexOf(';');
+      int secondSeparator = (firstSeparator >= 0)
+                              ? dimmerstate.indexOf(';', firstSeparator + 1)
+                              : -1;
+
+      if (firstSeparator > 0) {
+        gDisplayValues.dimmerReported =
+            dimmerstate.substring(0, firstSeparator).toInt();
+
+        if (secondSeparator > firstSeparator) {
+          gDisplayValues.temperature =
+              dimmerstate.substring(firstSeparator + 1, secondSeparator);
+        }
+        else {
+          gDisplayValues.temperature =
+              dimmerstate.substring(firstSeparator + 1);
+        }
+
+        gDisplayValues.dimmerCommOk = true;
+        gDisplayValues.dimmerLastOkMs = millis();
+
+        Serial.printf("[DIMMER STATE] POWER=%d TEMP=%s OK=1\n",
+                      gDisplayValues.dimmerReported,
+                      gDisplayValues.temperature.c_str());
+      }
+      else {
+        gDisplayValues.dimmerCommOk = false;
+        Serial.println("[DIMMER STATE] invalid payload");
+      }
+    }
+    else {
+      Serial.print("Error code: ");
+      Serial.println(httpResponseCode);
+      gDisplayValues.dimmerCommOk = false;
+    }
+
+    httpdimmer.end();
+
+    // Refresh every GETTEMPREFRESH seconds.
+    vTaskDelay(GETTEMPREFRESH * 1000 / portTICK_PERIOD_MS);
   }
-  else {
-    Serial.print("Error code: ");
-    Serial.println(httpResponseCode);
-  }
-  // Free resources
-  httpdimmer.end();
-
-
-// hash temp 
-int starttemp = dimmerstate.indexOf(";"); 
-dimmerstate = dimmerstate.substring(starttemp+1); 
-
-gDisplayValues.temperature = dimmerstate; 
- Serial.println("temperature " + dimmerstate);
-
-// refresh every GETTEMPREFRESH seconds 
-vTaskDelay(GETTEMPREFRESH * 1000 / portTICK_PERIOD_MS);
-  }
-
 }
 
-
 #endif
-
-
-
