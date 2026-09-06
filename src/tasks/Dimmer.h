d4b56fee9720dac66bbd1744ccf97aa5948b0d23
Cedric Poirson <42615723+CedricPoirson@users.noreply.github.com>
2026-09-06 09:19:27 +0200

    Add Fronius Zero Grid simulation to dimmer task

--- src/tasks/Dimmer.h
+++ src/tasks/Dimmer.h
@@ -5,31 +5,38 @@
     #include "../config/config.h"
     #include "../config/enums.h"
     #include "../functions/dimmerFunction.h"
-
+    #include "../functions/froniusZeroGrid.h"
 
 
 extern DisplayValues gDisplayValues;
 
 /**
  * Task: Modifier le dimmer en fonction de la production
- * 
+ *
  * récupère les informations, conso ou injection et fait varier le dimmer en conséquence
- * 
+ *
  */
 void updateDimmer(void * parameter){
   for (;;){
   gDisplayValues.task = true;
 #if WIFI_ACTIVE == true
+
+    // Simulation Fronius Zero Grid uniquement
+    // Aucune commande triac envoyée à ce stade
+    if (gDisplayValues.froniusup == true) {
+      froniusZeroGridSimulation();
+    }
+
     dimmer();
-    
+
     /*
     /// si changement à faire
     if  (gDisplayValues.change != 0 ) {
         Serial.println(F("changement des valeurs dimmer-MQTT"));
         // envoie de l'information au dimmer et au serveur MQTT ( mosquito ou autre )
-        dimmer_change(); 
-    }*/ 
-   
+        dimmer_change();
+    }*/
+
 #endif
     gDisplayValues.task = false;
    // Sleep for 5 seconds, avant de refaire une analyse
