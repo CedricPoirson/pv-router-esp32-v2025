373cb003246d85b6abbb9b1d1afd09e7a5a1f391
Cedric Poirson <42615723+CedricPoirson@users.noreply.github.com>
2026-09-06 19:58:46 +0200

    Parse RobotDyn 20260514 /state JSON

--- src/tasks/gettemp.h
+++ src/tasks/gettemp.h
@@ -2,12 +2,12 @@
 #define TASK_GET_TEMP
 
 #include <Arduino.h>
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
 
 #include "../config/config.h"
 #include "../config/enums.h"
 #include "../functions/Mqtt_http_Functions.h"
-
-HTTPClient httpdimmer;
 
 extern DisplayValues gDisplayValues;
 
@@ -17,77 +17,92 @@ extern volatile bool gDisplayForceRefresh;
 
 #define DIMMER_STATE_POLL_SYNC_MS    5000UL
 #define DIMMER_STATE_POLL_CATCHUP_MS 2000UL
-#define DIMMER_STATE_HTTP_TIMEOUT_MS  1200UL
+#define DIMMER_STATE_HTTP_TIMEOUT_MS  500UL
 
 void GetDImmerTemp(void * parameter){
+  (void)parameter;
+
   bool linkStateKnown = false;
   bool previousLinkOk = false;
 
-  for (;;){
+  for (;;) {
     unsigned long nextPollMs = DIMMER_STATE_POLL_CATCHUP_MS;
     bool currentLinkOk = false;
     String errorReason = "unknown";
 
-    String baseurl = "/state";
-    httpdimmer.begin(String(config.dimmer), 80, baseurl);
+    HTTPClient httpdimmer;
+    httpdimmer.setConnectTimeout(DIMMER_STATE_HTTP_TIMEOUT_MS);
     httpdimmer.setTimeout(DIMMER_STATE_HTTP_TIMEOUT_MS);
-    int httpResponseCode = httpdimmer.GET();
 
-    if (httpResponseCode == HTTP_CODE_OK) {
-      String dimmerstate = httpdimmer.getString();
+    if (httpdimmer.begin(String(config.dimmer), 80, "/state")) {
+      const int httpResponseCode = httpdimmer.GET();
 
-      // Expected payload: POWER;temperature;...
-      int firstSeparator = dimmerstate.indexOf(';');
-      int secondSeparator = (firstSeparator >= 0)
-                              ? dimmerstate.indexOf(';', firstSeparator + 1)
-                              : -1;
+      if (httpResponseCode == HTTP_CODE_OK) {
+        const String payload = httpdimmer.getString();
+        StaticJsonDocument<1024> doc;
+        const DeserializationError error = deserializeJson(doc, payload);
 
-      if (firstSeparator > 0) {
-        gDisplayValues.dimmerReported =
-            dimmerstate.substring(0, firstSeparator).toInt();
+        if (!error && doc.containsKey("dimmer") && doc.containsKey("onoff")) {
+          gDisplayValues.dimmerReported = constrain(doc["dimmer"] | 0, 0, 100);
+          gDisplayValues.dimmerCommandReported = constrain(doc["commande"] | 0, 0, 100);
 
-        if (secondSeparator > firstSeparator) {
-          gDisplayValues.temperature =
-              dimmerstate.substring(firstSeparator + 1, secondSeparator);
+          const char *dallas0 = doc["dallas0"] | nullptr;
+          const char *temperature = doc["temperature"] | nullptr;
+          if (dallas0 && dallas0[0] != '\0')
+            gDisplayValues.temperature = dallas0;
+          else if (temperature)
+            gDisplayValues.temperature = temperature;
+
+          gDisplayValues.dimmerPower = doc["power"] | 0.0f;
+          gDisplayValues.dimmerPtotal = doc["Ptotal"] | 0.0f;
+          gDisplayValues.dimmerRssi = doc["RSSI"] | -127;
+          gDisplayValues.dimmerOn = doc["onoff"] | false;
+
+          gDisplayValues.dimmerAlert = String((const char *)(doc["alerte"] | ""));
+          gDisplayValues.dimmerVersion = String((const char *)(doc["version"] | ""));
+          gDisplayValues.dimmerAlarm =
+              gDisplayValues.dimmerAlert.length() > 0 &&
+              !gDisplayValues.dimmerAlert.equalsIgnoreCase("RAS");
+
+          gDisplayValues.dimmerCommOk = true;
+          gDisplayValues.dimmerLastOkMs = millis();
+          currentLinkOk = true;
+
+          int commandedDimmer = constrain(gDisplayValues.dimmer, 0, 100);
+          const bool synced =
+              abs(commandedDimmer - gDisplayValues.dimmerReported) <= 2;
+          nextPollMs = synced ? DIMMER_STATE_POLL_SYNC_MS
+                              : DIMMER_STATE_POLL_CATCHUP_MS;
+
+          if (!linkStateKnown || !previousLinkOk) {
+            Serial.printf("[DIMMER] LINK OK ACTUAL=%d%% CMD=%d%% TEMP=%s C RSSI=%d\n",
+                          gDisplayValues.dimmerReported,
+                          gDisplayValues.dimmerCommandReported,
+                          gDisplayValues.temperature.c_str(),
+                          gDisplayValues.dimmerRssi);
+          }
+
+          if (gDisplayValues.dimmerAlarm) {
+            Serial.printf("[DIMMER] ALARM: %s\n", gDisplayValues.dimmerAlert.c_str());
+          }
         }
         else {
-          gDisplayValues.temperature =
-              dimmerstate.substring(firstSeparator + 1);
-        }
-
-        gDisplayValues.dimmerCommOk = true;
-        gDisplayValues.dimmerLastOkMs = millis();
-        currentLinkOk = true;
-
-        int commandedDimmer = gDisplayValues.dimmer;
-        if (commandedDimmer < 0) commandedDimmer = 0;
-        if (commandedDimmer > 100) commandedDimmer = 100;
-
-        int reportedDimmer = gDisplayValues.dimmerReported;
-        if (reportedDimmer < 0) reportedDimmer = 0;
-        if (reportedDimmer > 100) reportedDimmer = 100;
-
-        const bool synced = abs(commandedDimmer - reportedDimmer) <= 2;
-        nextPollMs = synced ? DIMMER_STATE_POLL_SYNC_MS
-                            : DIMMER_STATE_POLL_CATCHUP_MS;
-
-        if (!linkStateKnown || !previousLinkOk) {
-          Serial.printf("[DIMMER] LINK OK POWER=%d%% TEMP=%s C\n",
-                        reportedDimmer,
-                        gDisplayValues.temperature.c_str());
+          gDisplayValues.dimmerCommOk = false;
+          errorReason = error ? String("JSON ") + error.c_str()
+                              : "invalid /state JSON";
         }
       }
       else {
         gDisplayValues.dimmerCommOk = false;
-        errorReason = "invalid /state payload";
+        errorReason = "HTTP " + String(httpResponseCode);
       }
+
+      httpdimmer.end();
     }
     else {
       gDisplayValues.dimmerCommOk = false;
-      errorReason = "HTTP " + String(httpResponseCode);
+      errorReason = "HTTP begin failed";
     }
-
-    httpdimmer.end();
 
     if (!currentLinkOk && (!linkStateKnown || previousLinkOk)) {
       Serial.printf("[DIMMER] LINK ERROR (%s)\n", errorReason.c_str());
