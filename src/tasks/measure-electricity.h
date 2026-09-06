84da8b0bd924a538c0d33007c6a0346cbf0d48a1
Cedric Poirson <42615723+CedricPoirson@users.noreply.github.com>
2026-09-06 06:57:19 +0200

    Fix Fronius online status detection after API failure

--- src/tasks/measure-electricity.h
+++ src/tasks/measure-electricity.h
@@ -12,59 +12,54 @@
 
 // Fronius Inverter
 #include "HTTPClient.h"
-#include <ArduinoJson.h>  // Make sure this is included for JSON handling
+#include <ArduinoJson.h>
 
 extern DisplayValues gDisplayValues;
-extern Config config; 
+extern Config config;
 
 int Pow_mqtt_send = 0;
 
 void measureElectricityf(void * parameter)
 {
     for(;;){
-        long start = millis();
 
         #if WIFI_ACTIVE == true
             HTTPClient http;
             String url = "http://" + String(IP_FRONIUS) + "/solar_api/v1/GetPowerFlowRealtimeData.fcgi";
-            String url2 = "http://" + String(IP_FRONIUS) + "/solar_api/v1/GetPowerFlowRealtimeData.fcgi";
-            
+
+            gDisplayValues.froniusup = false;
+
             http.begin(url);
             int httpCode = http.GET();
 
             Serial.print("httpCode / function measure: ");
             Serial.println(httpCode);
 
-            #if(httpCode == HTTP_CODE_OK) 
+            if (httpCode == HTTP_CODE_OK) {
                 String payload = http.getString();
-                DynamicJsonDocument doc(900);
+                DynamicJsonDocument doc(1500);
                 DeserializationError error = deserializeJson(doc, payload);
 
-                long generatedPower = doc["Body"]["Data"]["Inverters"]["1"]["P"];
-                gDisplayValues.production  = generatedPower;
-            #else
-                gDisplayValues.froniusup = false;
-                Serial.println("gDisplayValues.froniusup = false");
-            #endif
+                if (!error) {
+                    if (doc["Body"]["Data"].containsKey("Site")) {
+                        long gridPower = doc["Body"]["Data"]["Site"]["P_Grid"];
+                        gDisplayValues.watt = gridPower;
+                        gDisplayValues.froniusup = true;
+                    }
+
+                    if (doc["Body"]["Data"]["Inverters"].containsKey("1")) {
+                        long generatedPower = doc["Body"]["Data"]["Inverters"]["1"]["P"];
+                        gDisplayValues.production = generatedPower;
+                    }
+                } else {
+                    Serial.println("Fronius JSON error");
+                }
+            } else {
+                Serial.println("Fronius API unavailable");
+            }
+
             http.end();
 
-            HTTPClient http2;
-            http2.begin(url2);
-            httpCode = http2.GET();
-            #if(httpCode == HTTP_CODE_OK) 
-                String payload2 = http2.getString();
-                DynamicJsonDocument doc2(1500);
-                error = deserializeJson(doc2, payload2);
-
-                long generatedPower2 = doc2["Body"]["Data"]["Site"]["P_Grid"];
-                gDisplayValues.watt  =  generatedPower2;
-                gDisplayValues.froniusup = true;
-            #else
-                gDisplayValues.froniusup = false;
-            #endif
-            http2.end();
-            Serial.print("generatedPower2 / function measure: ");
-            Serial.println(generatedPower2);
             Serial.print("gDisplayValues.production / function measure: ");
             Serial.println(gDisplayValues.production);
             Serial.print("gDisplayValues.watt / function measure: ");
@@ -73,18 +68,16 @@ void measureElectricityf(void * parameter)
             Serial.println(gDisplayValues.froniusup);
         #endif
 
-        long end = millis();
-
         #if WIFI_ACTIVE == true
             Pow_mqtt_send++;
             if (Pow_mqtt_send > 10) {
-                Mqtt_send(String(config.IDX), String(int(gDisplayValues.watt)));  
+                Mqtt_send(String(config.IDX), String(int(gDisplayValues.watt)));
                 Pow_mqtt_send = 0;
             }
         #endif
-        
-        vTaskDelay(2000 / portTICK_PERIOD_MS);  // Delay for 2 seconds to avoid overloading the system
-    }    
+
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
+    }
 }
 
 #endif
